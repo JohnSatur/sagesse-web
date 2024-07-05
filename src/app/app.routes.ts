3e57972e477dd9e7806f666280bc5b76d11d25e3
John Satur <animashorse3@yahoo.com.mx>
2024-07-05 14:07:48 -0600

    Creación del proyecto y sistema de rutas básico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { OfferComponent } from './pages/offer/offer.component';
+import { EnrollmentComponent } from './pages/enrollment/enrollment.component';
+import { StudentLifeComponent } from './pages/student-life/student-life.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'quienes-somos', component: AboutComponent },
+    { path: 'oferta-educativa', component: OfferComponent },
+    { path: 'admisiones', component: EnrollmentComponent },
+    { path: 'vida-estudiantil', component: StudentLifeComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contacto', component: ContactComponent },
+    { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/student-life/student-life.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-life',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-life.component.html',
+  styleUrl: './student-life.component.scss'
+})
+export class StudentLifeComponent {
+
+}
