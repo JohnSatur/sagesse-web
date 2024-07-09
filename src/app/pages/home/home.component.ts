7bc858132004d44bf742d12412d327ae567977d7
John Satur <animashorse3@yahoo.com.mx>
2024-07-09 16:55:23 -0600

    Swiper instalado y funcionando

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { FeatureComponent } from '../../components/feature/feature.component';
 import { LevelCardComponent } from '../../components/level-card/level-card.component';
+
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, ButtonComponent, FeatureComponent, LevelCardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
