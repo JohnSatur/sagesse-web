ca49fd7a0498567ff2173972ee6254a9d1be6a4a
John Satur <animashorse3@yahoo.com.mx>
2024-07-10 11:39:33 -0600

    Swiper funcionando, pero no acomodado :(

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { FeatureComponent } from '../../components/feature/feature.component';
 import { LevelCardComponent } from '../../components/level-card/level-card.component';
 
-import { register } from 'swiper/element/bundle';
+import { register, SwiperContainer } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
 register();
 
 @Component({
@@ -16,5 +17,18 @@ register();
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  swiperElement = signal<SwiperContainer | null>(null);
+
+  ngOnInit(): void {
+    const swiperElementConstructor = document.querySelector('swiper-container');
+    
+    const swiperOptions: SwiperOptions = {
+      slidesPerView: 3,
+      spaceBetween: 24
+    };
   
+    Object.assign(swiperElementConstructor!, swiperOptions);
+    this.swiperElement.set(swiperElementConstructor as SwiperContainer);
+    this.swiperElement()?.initialize();
+  }
 }
