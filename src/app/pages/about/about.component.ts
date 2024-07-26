b7feaf6814e35ba1cd08bb559cd124845073f479
John Satur <animashorse3@yahoo.com.mx>
2024-07-26 14:25:12 -0600

    Página 'Quiénes Somos' en desktop

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactLinkComponent } from '../../components/contact-link/contact-link.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ContactLinkComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
