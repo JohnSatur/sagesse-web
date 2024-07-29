60b676f9fb2fc1ce54212bbffc3cc6c6de538a88
John Satur <animashorse3@yahoo.com.mx>
2024-07-29 10:00:04 -0600

    Sección de oferta educativa para escritorio

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactLinkComponent } from '../../components/contact-link/contact-link.component';
 
 @Component({
   selector: 'app-offer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ContactLinkComponent],
   templateUrl: './offer.component.html',
   styleUrl: './offer.component.scss'
 })
