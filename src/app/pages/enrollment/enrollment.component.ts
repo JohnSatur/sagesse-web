5933f84c8b0ec9616a98607744857d20677f3a94
John Satur <animashorse3@yahoo.com.mx>
2024-07-30 10:00:34 -0600

    Página de admisiones para escritorio

--- src/app/pages/enrollment/enrollment.component.ts
+++ src/app/pages/enrollment/enrollment.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactLinkComponent } from '../../components/contact-link/contact-link.component';
 
 @Component({
   selector: 'app-enrollment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ContactLinkComponent],
   templateUrl: './enrollment.component.html',
   styleUrl: './enrollment.component.scss'
 })
