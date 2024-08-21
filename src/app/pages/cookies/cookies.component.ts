f2b6cc24428ab5dbdf13268a2eed68a7cc2b7ac6
John Satur <animashorse3@yahoo.com.mx>
2024-08-21 12:42:17 -0600

    Enrollment responsivo

---
+++ src/app/pages/cookies/cookies.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cookies.component.html',
+  styleUrl: './cookies.component.scss'
+})
+export class CookiesComponent {
+  
+}
---
+++ src/app/pages/privacy/privacy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy.component.html',
+  styleUrl: './privacy.component.scss'
+})
+export class PrivacyComponent {
+
+}
