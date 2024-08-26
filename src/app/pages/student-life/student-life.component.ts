94a6c2b7baf3244fa57a8ecd5d363c192fbb6d00
John Satur <animashorse3@yahoo.com.mx>
2024-08-26 10:38:46 -0600

    Vida estudiantil responsivo

--- src/app/pages/student-life/student-life.component.ts
+++ src/app/pages/student-life/student-life.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactLinkComponent } from '../../components/contact-link/contact-link.component';
 
 @Component({
   selector: 'app-student-life',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ContactLinkComponent],
   templateUrl: './student-life.component.html',
   styleUrl: './student-life.component.scss'
 })
