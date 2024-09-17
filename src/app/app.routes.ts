c708e00ded50d49fc142de022aa8e6e612c6cef8
John Satur <animashorse3@yahoo.com.mx>
2024-09-17 12:32:46 -0600

    Modo coming soon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,17 @@ import { BlogComponent } from './pages/blog/blog.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PrivacyComponent } from './pages/privacy/privacy.component';
 import { CookiesComponent } from './pages/cookies/cookies.component';
+import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'quienes-somos', component: AboutComponent },
-    { path: 'oferta-educativa', component: OfferComponent },
-    { path: 'admisiones', component: EnrollmentComponent },
-    { path: 'vida-estudiantil', component: StudentLifeComponent },
-    { path: 'blog', component: BlogComponent },
-    { path: 'contacto', component: ContactComponent },
-    { path: 'privacy', component: PrivacyComponent },
-    { path: 'cookies', component: CookiesComponent },
+    { path: '', component: ComingSoonComponent },
+    // { path: 'quienes-somos', component: AboutComponent },
+    // { path: 'oferta-educativa', component: OfferComponent },
+    // { path: 'admisiones', component: EnrollmentComponent },
+    // { path: 'vida-estudiantil', component: StudentLifeComponent },
+    // { path: 'blog', component: BlogComponent },
+    // { path: 'contacto', component: ContactComponent },
+    // { path: 'privacy', component: PrivacyComponent },
+    // { path: 'cookies', component: CookiesComponent },
     { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/coming-soon/coming-soon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './coming-soon.component.html',
+  styleUrl: './coming-soon.component.scss'
+})
+export class ComingSoonComponent {
+
+}
